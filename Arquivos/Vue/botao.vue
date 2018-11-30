bdee67c143604c46930d34db4ccc8dac6b446435
Raniel <whyfck@gmail.com>
2018-11-29 22:38:42 -0200

    :D

---
+++ Arquivos/Vue/botao.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">    
+    <button class="botao"> {{ mensagem }} </button>
+  </div>    
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      mensagem: 'Apenas um botão sem malícia nenhuma.'
+    }
+  }
+}
+</script>
+
+<style>
+.botao {
+  background-color: #5e7fba;
+  color: white;
+  border: none;
+  border-radius: 50px;
+  padding: 10px 30px;
+  transition-duration: 0.3s;
+}
+
+  .botao:hover {
+    background-color: #4f5283;
+    color: #ffff88;
+  }
+</style>
