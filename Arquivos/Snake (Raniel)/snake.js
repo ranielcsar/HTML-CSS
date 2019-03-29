a90d18b71071a776b9828aaf552669fc0aed76b5
Raniel <whyfck@gmail.com>
2019-03-29 02:31:53 -0300

    muita coisa mudada

--- Arquivos/Snake (Raniel)/snake.js
+++ Arquivos/Snake (Raniel)/snake.js
@@ -1,104 +1,7 @@
-var contexto;
-var label;
-var canvas, altura, largura, escala;
-var FPS;
-var playing;
-var snake;
-
-var tecla = {
-   esquerda: 37,
-   cima: 38,
-   direita: 39,
-   baixo: 40
-};
-
-window.addEventListener("keydown", entradaUsuario);
-window.addEventListener("resize", resizeWindow);
-
-function entradaUsuario(evento)
-{
-   if (!playing && (evento.keyCode == tecla.esquerda || evento.keyCode == tecla.cima || 
-   evento.keyCode == tecla.direita || evento.keyCode == tecla.baixo))
-   {
-      playing = true;
-   }
-
-   switch (evento.keyCode)
-   {
-      case tecla.esquerda:
-         snake.direcao = [-1, 0];
-         break;
-
-      case tecla.cima:
-         snake.direcao = [0, -1];
-         break;
-
-      case tecla.direita:
-         snake.direcao = [1, 0];
-         break;
-
-      case tecla.baixo:
-         snake.direcao = [0, 1];
-         break;
-   }
-}
-
-function resizeWindow() // função para deixar o jogo responsivo
-{
-   largura = window.innerWidth;
-   altura = window.innerHeight;
-
-   canvas.width = largura;
-   canvas.height = altura;
-
-   escala = Math.max(Math.floor(largura / 50), Math.floor(altura / 50)); 
-   // usado para dividir linhas e colunas dependendo do tamanho da tela
-}
-
-function novoJogo()
-{
-   snake = new Snake();
-
-   label = new playLabel();
-
-   playing = false;
-}
-
-function iniciaJogo()
-{
-   canvas = document.createElement("canvas"); // cria elemento canvas
-
-   resizeWindow(); // identifica o tamanho da tela antes de colocar o "canvas" no body do html
-
-   document.body.appendChild(canvas); // adiciona "canvas" ao body do html
-
-   contexto = canvas.getContext("2d");
-
-   FPS = 15;
-
-   novoJogo();
-   rodarJogo();
-}
-
-function playLabel()
-{
-   this.text = "Pressione qualquer seta para jogar! :D";
-   this.color = "#C8C8C8";
-
-   this.draw = function()
-   {
-      contexto.fillStyle = this.color;
-      contexto.font = escala + 10 + "px Arial";
-      contexto.fillText(this.text, largura / 2 - contexto.measureText(this.text).width / 2, altura / 2);
-   }
-}
-
 function Snake()
 {
                // x   y
    this.body = [[10, 10], [10, 11], [10, 12]]; // vetor de vetores
-   this.color = "#9aec99";   
-   this.colorLine = "#333333";   
    this.direcao = [0, -1];
 
    this.update = function()
@@ -110,22 +13,22 @@ function Snake()
 
       if (!playing)
       {
-         if (this.direcao[1] == -1 && nextPosition[1] <= (altura * 0.1 / escala))
+         if (this.direcao[1] == -1 && nextPosition[1] <= (altura * 0.1 / tamanho))
          {
             this.direcao = [1, 0];
          }
 
-         else if (this.direcao[0] == 1 && nextPosition[0] >= (largura * 0.9 / escala))
+         else if (this.direcao[0] == 1 && nextPosition[0] >= (largura * 0.9 / tamanho))
          {
             this.direcao = [0, 1];
          }
 
-         else if (this.direcao[1] == 1 && nextPosition[1] >= (altura * 0.9 / escala))
+         else if (this.direcao[1] == 1 && nextPosition[1] >= (altura * 0.9 / tamanho))
          {
             this.direcao = [-1, 0];
          }
 
-         else if (this.direcao[0] == -1 && nextPosition[0] <= (largura * 0.2 / escala))
+         else if (this.direcao[0] == -1 && nextPosition[0] <= (largura * 0.2 / tamanho))
          {
             this.direcao = [0, -1];         
          }
@@ -140,43 +43,22 @@ function Snake()
 
    this.draw = function()
    {
-      contexto.fillStyle = this.color;     
-      contexto.strokeStyle = this.colorLine;      
+      for (let i = 0; i < this.body.length; i++)
+      {       
+         lapis.fillStyle = (i == 0) ? "#A8F876" : "#194B2B";
 
-      for (var i = 0; i < this.body.length; i++)
-      {         
-         contexto.fillRect(this.body[i][0] * escala, this.body[i][1] * escala, escala, escala); // pega coord. em x/y para snake         
-         contexto.strokeRect(this.body[i][0] * escala, this.body[i][1] * escala, escala, escala);         
+         lapis.fillRect(this.body[i][0] * tamanho, this.body[i][1] * tamanho, tamanho, tamanho);
+         lapis.strokeRect(this.body[i][0] * tamanho, this.body[i][1] * tamanho, tamanho, tamanho);         
       }
    }
-}
 
-function atualiza()
-{
-   snake.update();
-}
-
-function checaParedes()
-{
-   
-}
-
-function rodarJogo()
-{
-   atualiza();
-   desenhaJogo();
-
-   setTimeout(rodarJogo, 1000 / FPS); // divide 1000 (milissegundos) por 15 (FPS)
-}
-
-function desenhaJogo()
-{
-   contexto.clearRect(0, 0, largura, altura); // limpa tela
-
-   snake.draw();
-
-   if (!playing)
+   this.comeu = function(fruta)
    {
-      label.draw();
+      if (this.body[0][0] === fruta.x && this.body[0][1] === fruta.y)
+      {
+         return true;
+      }
+
+      return false;
    }
 }
